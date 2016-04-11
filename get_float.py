e270c618a70ec476088a94f17c62979ec84e8c91
louiswi <lu814478913@gmail.com>
2016-04-11 19:33:18 +0800

    新文件：   csv2html.py 	新文件：   get_float.py 	新文件：   quadratic.py

---
+++ get_float.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+import sys
+def get_float(msg,allow_zero):
+    x = None
+    while x is None:
+        try:
+            x = float(input(msg))
+            if not allow_zero and abs(x) < sys.float_info.epsilon:
+                print("zero is not allow")
+                x = None
+        except ValueError as err:
+            print(err)
+    return x
